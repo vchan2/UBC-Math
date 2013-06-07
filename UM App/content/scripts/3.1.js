f811cdb75211436580013669fbd37c4e22b887c5
Sebastian Valdivia <sebastianvaldivia@host213-168.resnet.ubc.ca>
2013-06-06 17:08:00 -0700

    Created Section 2 - Multiplying

---
+++ UM App/content/scripts/3.1.js
@@ -0,0 +1,159 @@
+var xReg = /x/;  var yReg = /y/;
+
+function checkPrac1(f) {
+    var inCoef = f.aAns1.value;  // input is 
+    var inXexp = f.pAns1.value;  //   inCoef x^inXexp y^inYexp (inTerm[0] + 
+    var inYexp = f.qAns1.value;  //             inTerm[1])
+    var inTerm = new Array(f.bAns1.value, f.cAns1.value);
+
+    var errs = new String("[1] x^[2] y^[3] ( [4] + [5] )");
+
+// easy stuff: did they get the right factored term?
+    if ( (inCoef == '' && a != 1) || (inCoef != '' && inCoef != a) ) { 
+        errs = errs.replace(/1/, "*"); 
+    }
+    if ( (inXexp == '' && b != 1) || (inXexp != '' && inXexp != b) ) { 
+        errs = errs.replace(/2/, "*"); 
+    }
+    if ( (inYexp == '' && c != 1) || (inYexp != '' && inYexp != c) ) { 
+        errs = errs.replace(/3/, "*"); 
+    }
+
+// now try to figure out what's going on with the rest of the problem
+//    correct terms are d x^g and h y^j
+    var matched = 0;
+    if ((matched = checkTerm(inTerm[0], d, g, h, j, matched)) == 0) {
+        errs = errs.replace(/4/, "*");
+    } 
+    if ((matched = checkTerm(inTerm[1], d, g, h, j, matched)) == 0) {
+        errs = errs.replace(/5/, "*");
+    } 
+
+    errs = errs.replace( /[0-9]/g, " " );
+    if ( errs.match(/\*/) != null ) {
+        f.ansTxt1.value='Hmm...  Check "*"s: ' + errs;
+    } else {
+        f.ansTxt1.value='Correct!  Try the next practice problem!';
+    }
+}
+
+function checkTerm(ans,c1,r1,c2,r2,match) {
+  // check to see if term looks like c1 x^r1 or c2 y^r2
+
+  // special case: one of the terms is constant
+    var xReg2 = xReg;  var yReg2 = yReg;
+    if ( r1 == 0 ) { 
+        xReg2 = /^[0-9]$/;
+    } else if ( r2 == 0 ) {
+        yReg2 = /^[0-9]$/;
+    }
+
+    if ( (xReg2.test(ans) && yReg2.test(ans)) ||
+         (! xReg2.test(ans) && ! yReg2.test(ans)) ) { return 0; }
+
+    var term = /(-?[0-9]+)? *[xy](\^([0-9]+))?/;
+
+    if ( (xReg2.test(ans) && match == 1) || (yReg2.test(ans) && match == 2) ) {
+        return 0;
+    } else if ( xReg2.test(ans) ) {
+	var matches;
+	matches = term.exec(ans);
+	if ( matches == null ) {
+	    if ( r1 != 0 || (r1 == 0 && ans != c1) ) {
+	        return 0;
+            } else {
+		return 1;
+	    }
+        } else {
+            if ( ( matches[1] == c1 || 
+	           ((matches[1] == null || matches[1] == '') && c1 == 1) ) && 
+ 	         ( matches[3] == r1 || (matches[2] == null && r1 == 1) ) ) {
+                return 1;
+	    } else { return 0; }
+        }
+    } else if ( yReg2.test(ans) ) {
+	var matches;
+	matches = term.exec(ans);
+	if ( matches == null ) {
+	    if ( r2 != 0 || (r2 == 0 && ans != c2) ) {
+	        return 0;
+	    } else {
+	        return 2;
+            }
+        } else {
+            if ( ( matches[1] == c2 || 
+	           ((matches[1] == null || matches[1] == '') && c2 == 1) ) && 
+	         ( matches[3] == r2 || 
+		   ((matches[2] == null || matches[2] == '') && r2 == 1) ) ) {
+	        return 2; 
+	    } else { return 0; }
+        }
+    } else {
+        return 0;
+    }
+}
+
+function checkPrac2(f) {
+    var inTerms = 
+        new Array(f.aAns2.value, f.bAns2.value, f.cAns2.value, f.dAns2.value);
+  // the answer is (q x^r + s y)(t y^u + v): check each pair against
+  //    the corresponding possible pairs of values from inTerms
+    var matched = 0;  var ok = false;
+    errs = '([1] + [2])([3] + [4])';
+
+  // first case: first input term matches one of the first binomial answers
+    if ( (matched = checkTerm(inTerms[0],q,r,s,1,matched)) > 0 ) {
+	var which = 3 - matched;
+	var mpat = new RegExp(matched);
+	errs = errs.replace(mpat, " ");
+  // then check to see if the second matches or not
+        if ( checkTerm(inTerms[1],q,r,s,1,matched) > 0 ) {
+	    var mpat = new RegExp(which);
+	    errs = errs.replace(mpat, " ");
+        }
+	errs = errs.replace(/[12]/g, "*");
+  // and then check the second binomial
+        matched = 0;
+        if ( (matched = checkTerm(inTerms[2],v,0,t,u,matched)) > 0 ) {
+	    which = 2 + 1*(matched%2 + 1);
+	    var mpat = new RegExp(which);
+	    errs = errs.replace(mpat, " ");
+        }
+	if ( (matched = checkTerm(inTerms[3],v,0,t,u,matched)) > 0 ) {
+	    which = 2 + 1*(matched%2 + 1);
+	    var mpat = new RegExp(which);
+	    errs = errs.replace(mpat, " ");
+        }
+	errs = errs.replace(/[34]/g, "*");
+    } else {
+  // ok, so the first input term didn't match the first binomial.  
+        if ( (matched = checkTerm(inTerms[0],v,0,t,u,matched)) > 0 ) {
+	    var which = matched%2 + 1*1;    // we swapped x & y in the call to
+	    var mpat = new RegExp(which);   //   checkTerm to keep "y" in the
+	    errs = errs.replace(mpat, " "); //   right place
+	} 
+	if ( (matched = checkTerm(inTerms[1],v,0,t,u,matched)) > 0 ) {
+	    var which = matched%2 + 1*1;
+	    var mpat = new RegExp(which);
+	    errs = errs.replace(mpat, " ");
+        }
+	errs = errs.replace(/[12]/g, "*");
+        matched = 0;
+        if ( (matched = checkTerm(inTerms[2],q,r,s,1,matched)) > 0 ) {
+	    var which = matched+1*2;
+	    var mpat = new RegExp(which);
+	    errs = errs.replace(mpat, " ");
+        }
+	if ( (matched=checkTerm(inTerms[3],q,r,s,1,matched)) > 0 ) {
+	    var which = matched+1*2;
+	    var mpat = new RegExp(which);
+	    errs = errs.replace(mpat, " ");
+        }
+	errs = errs.replace(/[34]/g, "*");
+    }
+    if ( errs.match(/\*/) != null ) {
+        f.ansTxt2.value='Hmm... Check "*"s: ' + errs;
+    } else {
+        f.ansTxt2.value='Correct!  Click "Refresh" for more practice!';
+    }
+}
---
+++ UM App/content/scripts/3.2.js
@@ -0,0 +1,88 @@
+// javascript for precalculus tutorial section 3.2
+
+function checkPrac1(f) {
+// answer is (ax + b)^2, where it happens that b is always 1
+    var aans = f.aAns1.value;
+    var bans = f.bAns1.value;
+
+    var errs = '';
+    if ( ! (aans == a || (a == 1 && aans == '')) ) {
+        errs = pracError(errs, 'a');
+    }
+    if ( bans != 1 && bans != '' ) {
+        errs = pracError(errs, 'b');
+    }
+    if ( errs != '' ) {
+        f.ansTxt1.value = errs;
+    } else {
+        f.ansTxt1.value = 'Correct!  Try the next practice problem!';
+    }
+    return 1;
+}
+
+function checkPrac2(f) {
+// answer is (x^c - d x^g)^2
+    var cans = f.pAns2.value;
+    var dans = f.aAns2.value;
+    var gans = f.qAns2.value;
+
+    var errs = '';
+    if ( ! (dans == d || (d == 1 && dans == '')) ) {
+        errs = pracError(errs, 'b');
+    }
+    if ( ! (cans == c || (c == 1 && cans == '')) ) {
+        errs = pracError(errs, 'm');
+    }
+    if ( ! (gans == g || (g == 1 && gans == '')) ) {
+        errs = pracError(errs, 'n');
+    }
+    if ( errs != '' ) {
+        f.ansTxt2.value = errs;
+    } else {
+        f.ansTxt2.value = 'Correct!  Try the next practice problem!';
+    }
+    return 1;
+}
+
+function checkPrac3(f) {
+// answer is (h x^j + y^k) (hx^j - y^k)
+    var hans = f.aAns3.value;
+    var jans = f.pAns3.value;
+    var kans = f.qAns3.value;
+    var h2ans = f.bAns3.value;
+    var j2ans = f.rAns3.value;
+    var k2ans = f.sAns3.value;
+
+    var errs = ''
+    if ( hans != h2ans ) {
+        errs = 'Check your value(s) of a, which should be the same.';
+    } else if ( jans != j2ans ) {
+        errs = 'Check your value(s) of m, which should be the same.';
+    } else if ( kans != k2ans ) {
+        errs = 'Check your value(s) of n, which should be the same.';
+    }
+    if ( errs != '' ) {
+        f.ansTxt3.value = errs;
+        return 1;
+    } else {
+        if ( hans != h ) {
+	    errs = pracError(errs, 'a');
+	}
+	if ( ! (jans == j || ( j == 1 && jans == '' )) ) {
+	    errs = pracError(errs, 'm');
+	}
+	if ( ! (kans == k || ( k == 1 && kans == '' )) ) {
+	    errs = pracError(errs, 'n');
+	}
+	if ( errs != '' ) {
+	    f.ansTxt3.value = errs;
+	} else {
+	    f.ansTxt3.value = 'Correct!  Click "refresh" for more practice!';
+        }
+    }
+}
+
+function update(to,from) {
+    to.value = from.value;
+    return 1;
+}
